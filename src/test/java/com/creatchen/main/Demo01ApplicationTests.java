99e502af39c7a59cd4eb7899120916001f3f954e
ricado <1169833934@qq.com>
2020-06-06 17:06:32 +0800

    提交测试文件

--- src/test/java/com/creatchen/main/Demo01ApplicationTests.java
+++ src/test/java/com/creatchen/main/Demo01ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.creatchen.main;
 
+import com.creatchen.jenkins.sgy.PeopleSgy;
+import com.creatchen.strategy.StrategyFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class Demo01ApplicationTests {
 
+	@Autowired
+	private StrategyFactory strategyFactory;
+
 	@Test
 	void contextLoads() {
+		PeopleSgy peopleSgy = strategyFactory.getSgy("China", PeopleSgy.class);
+		log.info(peopleSgy.say());
 	}
 
 }
